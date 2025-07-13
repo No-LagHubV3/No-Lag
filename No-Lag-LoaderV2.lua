35102d1703333194096779ccab5e74fac7f3f09e
No-LagHubV3 <ryouji.gonzaga201@gmail.com>
2025-07-13 09:27:04 +0800

    Create No-Lag-LoaderV2.lua

---
+++ No-Lag-LoaderV2.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV2.lua"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/oncBy1yojB/0", true))()
